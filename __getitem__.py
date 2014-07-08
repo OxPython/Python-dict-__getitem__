c831a192e2095f13eef14035cac3ec2005a8d806
Emerson <root@emerson-N53SN>
2014-07-08 05:44:15 -0500

    Are there other ways to get data from a dictionary in Python?

--- __getitem__.py
+++ __getitem__.py
@@ -3,9 +3,9 @@ Created on Jul 2, 2014
 
 @author: viejoemer
 
-Are there other ways to get data into a dictionary in Python?
+Are there other ways to get data from a dictionary in Python?
 
-¿Hay otras maneras de obtener datos en un diccionario en Python?
+¿Hay otras maneras de obtener datos de un diccionario en Python?
 
 Python has a __getitem__ special method to get a items in a dictionary.
 '''
@@ -14,6 +14,26 @@ Python has a __getitem__ special method to get a items in a dictionary.
 d = {'three': 3, 'two': 2, 'one': 1}
 print(d)
 
+#get a value traditional.
+value = d['two']
+print(value)
+
 #Using a special method __setitem__
 value = d.__getitem__("one")
+print(value)
+
+#Definition of a dict into an other dict
+d = {'one': {'two': 2}}
+print(d)
+
+#get a value.
+value = d['one']
+print(value)
+
+#get a value.
+value = value['two']
+print(value)
+
+#get the value in one line.
+value = d['one']['two']
 print(value)
